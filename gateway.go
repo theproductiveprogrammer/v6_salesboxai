b7af75506ff7a3d65d62d5811f07065715afa0af
Charles L <charles.lobo@gmail.com>
2020-08-06 10:58:27 +1000

    feat: add API Gateway routing

--- gateway.go
+++ gateway.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"strings"
 )
@@ -26,14 +27,14 @@ func startServer(map_ map[string]string) {
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		routeReq(w, r, map_)
+		routeReq(r, w, map_)
 	})
 
 	fmt.Printf("\nServer started on port %s...\n", PORT)
 	http.ListenAndServe(":"+PORT, nil)
 }
 
-func routeReq(w http.ResponseWriter, r *http.Request, map_ map[string]string) {
+func routeReq(r *http.Request, w http.ResponseWriter, map_ map[string]string) {
 
 	setCORSHeaders(r, w)
 
@@ -44,13 +45,14 @@ func routeReq(w http.ResponseWriter, r *http.Request, map_ map[string]string) {
 
 	for k, v := range map_ {
 		if strings.HasPrefix(uri, k) {
-      dest := v + uri[len(k):]
+			dest := v + uri[len(k):]
 			fmt.Printf("routing '%s' to '%s'\n", uri, dest)
+			routeTo(dest, r, w)
 			return
 		}
 	}
 
-  fmt.Printf("failed routing '%s' (not found)\n", uri)
+	fmt.Printf("failed routing '%s' (not found)\n", uri)
 	w.WriteHeader(http.StatusNotFound)
 }
 
@@ -62,4 +64,43 @@ func setCORSHeaders(r *http.Request, w http.ResponseWriter) {
 	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 	w.Header().Set("Access-Control-Allow-Headers",
 		"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+}
+
+/*    outcome/
+ * We create a new request to the URI provided with the request
+ * method, headers, and body then we copy across the response
+ * body and headers.
+ */
+func routeTo(uri string, r *http.Request, w http.ResponseWriter) {
+	var req *http.Request
+	var resp *http.Response
+	var err error
+
+	defer r.Body.Close()
+
+	req, err = http.NewRequest(r.Method, uri, r.Body)
+	if err != nil {
+		w.WriteHeader(500)
+		return
+	}
+	for name, value := range r.Header {
+		for _, v := range value {
+			req.Header.Add(name, v)
+		}
+	}
+	client := &http.Client{}
+	resp, err = client.Do(req)
+	if err != nil {
+		w.WriteHeader(500)
+		return
+	}
+
+	for k, val := range resp.Header {
+		for _, v := range val {
+			w.Header().Add(k, v)
+		}
+	}
+	w.WriteHeader(resp.StatusCode)
+	io.Copy(w, resp.Body)
+	resp.Body.Close()
 }
