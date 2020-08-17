5d83d72ce5c967f1d66f96c7186b2ff0cc3027d8
Charles L <charles.lobo@gmail.com>
2020-08-17 17:37:26 +1000

    feat: show event as first/default toolbar item

--- src/index.js
+++ src/index.js
@@ -98,6 +98,7 @@ function toolbarIcon(store, i) {
 
 function getToolbar(store) {
   store.event('got/toolbar', [
+    { name: 'Event', icon: require('./event.svg') },
     { name: 'Send Email', icon: require('./email.svg') },
     { name: 'Adaptive', icon: require('./adaptive.svg') },
     { name: 'Chat', icon: require('./chat.svg') },
@@ -109,7 +110,6 @@ function getToolbar(store) {
     { name: 'Add To List', icon: require('./listadd.svg') },
     { name: 'Facebook', icon: require('./facebook.svg') },
     { name: 'Meeting', icon: require('./meeting.svg') },
-    { name: 'Event', icon: require('./event.svg') },
   ])
 }
 
