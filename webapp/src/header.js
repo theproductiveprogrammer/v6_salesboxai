612b47c60c8e73c2ba61c412e373e0e50aaafee4
Charles L <charles.lobo@gmail.com>
2020-08-24 10:55:18 +1000

    feat: show profile ikon

--- webapp/src/header.js
+++ webapp/src/header.js
@@ -4,5 +4,21 @@ const { h } = require('@tpp/htm-x')
 import './header.css'
 
 export function show(store, on) {
-  on.appendChild(h('img', { src: "salesboxai-logo.png" }))
+  let ikon = h('.ikon')
+  on.appendChild(h('img.logo', { src: "salesboxai-logo.png" }))
+  on.appendChild(ikon)
+
+
+  store.react('profile', profile => {
+    if(!profile) ikon.c()
+    else {
+      ikon.c(
+        h('.pic', profile.name[0]),
+        h('.info', [
+          h('.name', profile.name),
+          h('.tenant', profile.tenant.name)
+        ])
+      )
+    }
+  })
 }
