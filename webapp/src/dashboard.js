39ed9b6e51b9c60040dd29310ad931e6598cd6db
Charles L <charles.lobo@gmail.com>
2020-08-24 12:29:26 +1000

    feat: dashboard mockup

--- webapp/src/dashboard.js
+++ webapp/src/dashboard.js
@@ -4,5 +4,54 @@ const { h } = require('@tpp/htm-x')
 import './dashboard.css'
 
 export function show(store, on) {
-  on.appendChild(h('h1', "Dashboard"))
+  let dashboard = h('.dashboard')
+  on.appendChild(dashboard)
+
+  let leadcard = h('.card')
+  let workflowcard = h('.card')
+  let importcard = h('.card')
+
+  dashboard.c(
+    h('h1', "Home"),
+    leadcard.c(
+      h('.title', "Leads"),
+      h('.num', "14324"),
+      h('.row', [
+        h('.col', [
+          h('.label', "New"),
+          h('.new', "14312"),
+        ]),
+        h('.col', [
+          h('.label', "Hot"),
+          h('.hot', "12"),
+        ])
+      ]),
+      h('img.edit', { src: './edit.svg' })
+    ),
+    workflowcard.c(
+      h('.title', "Workflows"),
+      h('img', { src: './workflow.svg'}),
+      h('img.edit', { src: './edit.svg' })
+    ),
+    importcard.c(
+      h('.title', "Events"),
+      h('.line', [
+        h('.head', "Imports"),
+        h('.val', "12"),
+      ]),
+      h('.line', [
+        h('.head', "Clicks"),
+        h('.val', "8"),
+      ]),
+      h('.line', [
+        h('.head', "Email Opens"),
+        h('.val', "2"),
+      ]),
+      h('.line', [
+        h('.head', "Chats"),
+        h('.val', "77"),
+      ]),
+      h('img.edit', { src: './edit.svg' })
+    )
+  )
 }
