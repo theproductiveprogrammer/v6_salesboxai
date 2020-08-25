115de32926211f3dbd93ed3e8637689956a649cf
Charles L <charles.lobo@gmail.com>
2020-08-25 16:50:03 +1000

    feat: dashboard sketch

--- webapp/src/dashboard.js
+++ webapp/src/dashboard.js
@@ -11,47 +11,56 @@ export function show(store, on) {
   let workflowcard = h('.card')
   let importcard = h('.card')
 
+  leadcard.c(
+    h('.title', "Leads"),
+    h('.num', "14324"),
+    h('.row', [
+      h('.col', [
+        h('.label', "New"),
+        h('.new', "14312"),
+      ]),
+      h('.col', [
+        h('.label', "Hot"),
+        h('.hot', "12"),
+      ])
+    ]),
+    h('img.edit', { src: './edit.svg' })
+  )
+
+  workflowcard.c(
+    h('.title', "Workflows"),
+    h('img', { src: './workflow.svg'}),
+    h('img.edit', {
+      src: './edit.svg',
+      onclick: () => store.event('dash/workflow/go')
+    })
+  )
+
+  importcard.c(
+    h('.title', "Events"),
+    h('.line', [
+      h('.head', "Imports"),
+      h('.val', "12"),
+    ]),
+    h('.line', [
+      h('.head', "Clicks"),
+      h('.val', "8"),
+    ]),
+    h('.line', [
+      h('.head', "Email Opens"),
+      h('.val', "2"),
+    ]),
+    h('.line', [
+      h('.head', "Chats"),
+      h('.val', "77"),
+    ]),
+    h('img.edit', { src: './edit.svg' })
+  )
+
   dashboard.c(
     h('h1', "Home"),
-    leadcard.c(
-      h('.title', "Leads"),
-      h('.num', "14324"),
-      h('.row', [
-        h('.col', [
-          h('.label', "New"),
-          h('.new', "14312"),
-        ]),
-        h('.col', [
-          h('.label', "Hot"),
-          h('.hot', "12"),
-        ])
-      ]),
-      h('img.edit', { src: './edit.svg' })
-    ),
-    workflowcard.c(
-      h('.title', "Workflows"),
-      h('img', { src: './workflow.svg'}),
-      h('img.edit', { src: './edit.svg' })
-    ),
-    importcard.c(
-      h('.title', "Events"),
-      h('.line', [
-        h('.head', "Imports"),
-        h('.val', "12"),
-      ]),
-      h('.line', [
-        h('.head', "Clicks"),
-        h('.val', "8"),
-      ]),
-      h('.line', [
-        h('.head', "Email Opens"),
-        h('.val', "2"),
-      ]),
-      h('.line', [
-        h('.head', "Chats"),
-        h('.val', "77"),
-      ]),
-      h('img.edit', { src: './edit.svg' })
-    )
+    leadcard,
+    workflowcard,
+    importcard
   )
 }
