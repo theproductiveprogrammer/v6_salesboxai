e6fda0685ae0be108910b82557b2a7ef31815e20
Charles L <charles.lobo@gmail.com>
2020-08-29 10:40:57 +1000

    fix: signup page not showing

--- webapp/src/nav.js
+++ webapp/src/nav.js
@@ -3,16 +3,13 @@ const { h } = require('@tpp/htm-x')
 
 import './nav.css'
 
-let UNAUTHORIZED
 export function setup(rootstore) {
-  UNAUTHORIZED = () => rootstore.get('user') == null
 }
 
 export function init() {
 }
 
 export function reducer(state, type, payload) {
-  if(UNAUTHORIZED()) return 'login'
   switch(type) {
     case 'ac/unauthorized': return 'login'
     case 'ac/user': return 'dashboard'
