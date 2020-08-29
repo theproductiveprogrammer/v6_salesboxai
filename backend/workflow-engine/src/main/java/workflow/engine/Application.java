60f323aa6b3d641008ffdb236fc13c5435c22a11
Charles L <charles.lobo@gmail.com>
2020-08-29 21:58:16 +1000

    refactor: improve flow within application startup

--- backend/workflow-engine/src/main/java/workflow/engine/Application.java
+++ backend/workflow-engine/src/main/java/workflow/engine/Application.java
@@ -1,19 +1,35 @@
 package workflow.engine;
 
 import com.uber.cadence.worker.Worker;
+import io.micronaut.context.event.ApplicationEventListener;
 import io.micronaut.runtime.Micronaut;
+import io.micronaut.runtime.server.event.ServerStartupEvent;
+import workflow.engine.activities.Getter;
 
-public class Application {
+import javax.inject.Inject;
 
-    public static void main(String[] args) {
+public class Application implements ApplicationEventListener<ServerStartupEvent> {
+
+    public static void main(String[] args) { Micronaut.run(Application.class, args); }
+
+    @Inject
+    private Getter getter;
+
+    @Override
+    public void onApplicationEvent(ServerStartupEvent event) {
         registerWorkflowEngine();
-        Micronaut.run(Application.class, args);
     }
 
-    private static void registerWorkflowEngine() {
+    private void registerWorkflowEngine() {
         Worker.Factory factory = new Worker.Factory("salesboxai-domain");
         Worker worker = factory.newWorker("events-workflow-tasklist");
         worker.registerWorkflowImplementationTypes(SBEventWorkflow.class);
+        worker.registerActivitiesImplementations(getter);
         factory.start();
+    }
+
+    @Override
+    public boolean supports(ServerStartupEvent event) {
+        return true;
     }
 }
