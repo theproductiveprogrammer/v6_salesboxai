8e6ec52714dd54487acf40b307e0860b0c09027d
Charles L <charles.lobo@gmail.com>
2020-08-31 09:29:40 +1000

    feat: configure bizObjects client

--- backend/workflow-engine/src/main/java/workflow/engine/activities/Getter.java
+++ backend/workflow-engine/src/main/java/workflow/engine/activities/Getter.java
@@ -3,23 +3,28 @@ package workflow.engine.activities;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.client.RxHttpClient;
 import io.micronaut.http.client.annotation.Client;
+import io.micronaut.http.uri.UriBuilder;
 import workflow.engine.WorkflowStep;
 
 import javax.inject.Inject;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Getter implements IGetter {
 
     @Inject
-    @Client
-    RxHttpClient httpClient;
+    @Client("biz-objects")
+    RxHttpClient bizObjectsClient;
 
     @Override
     synchronized public List<WorkflowStep> getWorkflows(Long tenantId) {
-        return httpClient
+        URI uri = UriBuilder.of("/internal/workflows")
+                .queryParam("tenantId", tenantId)
+                .build();
+        return bizObjectsClient
                 .toBlocking()
-                .retrieve(HttpRequest.GET("http://localhost:6160/internal/workflows?&tenantId=" + tenantId), Res.class);
+                .retrieve(HttpRequest.GET(uri), Res.class);
     }
 
     public static class Res extends ArrayList<WorkflowStep> { }
