e149654faa9838f3180b7028a6b2123a615630f6
Charles L <charles.lobo@gmail.com>
2020-08-30 10:20:19 +1000

    feat: add dev scripts

---
+++ dev-scripts/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export JAVA8_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_231.jdk/Contents/Home/
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_231.jdk/Contents/Home/
+./bin/cassandra -f
